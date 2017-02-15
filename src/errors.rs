17d8e4fa505b93394e91f0d69dabb04c325e157f
Raphaël Lustin <raphael@lustin.fr>
2017-02-15 21:51:43 +0100

    Import Linter from Lugh
    Imported from https://github.com/rlustin/lugh at commit:
    da06e42779a9bd6968c6280b28ce3848c8acaf94

---
+++ src/errors.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, RustcEncodable)]
+pub struct LinterWarning {
+    pub message: &'static str,
+    pub start: usize,
+    pub end: usize
+}
+
+impl fmt::Display for LinterWarning {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            LinterWarning { ref message, ref start, ref end } => {
+                f.write_str(
+                    format!("Warning: {} ({}, {})", message, start, end).as_str()
+                )
+            },
+        }
+    }
+}
+
+impl Error for LinterWarning {
+    #[allow(unused_variables)]
+    fn description(&self) -> &str {
+        match *self {
+            LinterWarning { ref message, ref start, ref end } => { message },
+        }
+    }
+}
